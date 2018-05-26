a20a5f7ae62dbf3a5240082b0d6298501d2ace46
scuba10steve <steven.tompkins.jr@gmail.com>
2018-05-25 22:04:52 -0500

    Finalizing release step #6 (#18)

--- scripts/package.py
+++ scripts/package.py
@@ -21,23 +21,23 @@ def main(args):
     if os.path.exists(output_file):
         os.remove(output_file)
 
-    zipf = zipfile.ZipFile(os.path.abspath(output_file) + '.zip', 'w', zipfile.ZIP_BZIP2)
-    tarf = tarfile.open(os.path.abspath(output_file) + '.tar.bz2', 'w:bz2')
+    zipf = zipfile.ZipFile(output_file + '.zip', 'w', zipfile.ZIP_BZIP2)
+    tarf = tarfile.open(output_file + '.tar.bz2', 'w:bz2')
     # pylint: disable=unused-variable
 
-    for root, dirs, files in os.walk(PROJECT_DIR + '/src'):
+    for root, dirs, files in os.walk('src'):
         for file in files:
             zipf.write(os.path.join(root, file))
             tarf.add(os.path.join(root, file))
 
-    zipf.write(os.path.join(os.path.abspath(PROJECT_DIR), 'app.py'))
-    tarf.add(os.path.join(os.path.abspath(PROJECT_DIR), 'app.py'))
-    zipf.write(os.path.join(os.path.abspath(PROJECT_DIR), 'environment.yml'))
-    tarf.add(os.path.join(os.path.abspath(PROJECT_DIR), 'environment.yml'))
+    zipf.write('app.py')
+    tarf.add('app.py')
+    zipf.write('environment.yml')
+    tarf.add('environment.yml')
     zipf.close()
     tarf.close()
-    print('Package: \'' + zipf.filename + '\' created successfully!')
-    print('Package: \'' + tarf.name + '\' created successfully!')
+    print("Package: '" + os.path.abspath(zipf.filename) + "' created successfully!")
+    print("Package: '" + os.path.abspath(tarf.name) + "' created successfully!")
 
 
 if __name__ == "__main__":
